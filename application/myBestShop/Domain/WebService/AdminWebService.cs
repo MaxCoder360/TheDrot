a2d91edec4b215c8b346972b1d29931801e3fa4f
danisHaz <43248094+danisHaz@users.noreply.github.com>
2023-04-25 00:54:47 +0300

    fix add websockets

--- application/myBestShop/Domain/WebService/AdminWebService.cs
+++ application/myBestShop/Domain/WebService/AdminWebService.cs
@@ -69,7 +69,7 @@ namespace myBestShop.Domain.WebService
             {
                 if (computer != null)
                 {
-                    WebSocket socket = new WebSocket(computer.ip_adress);
+                    WebSocket socket = new WebSocket("ws://" + computer.ip_adress + "5050" + "/ToClient");
                     socket.OnMessage += onMessageWS;
                     socket.OnError += onErrorWS;
 
