178fc44489638b15a6f4efcdcb4bbbb22410d822
danisHaz <43248094+danisHaz@users.noreply.github.com>
2023-05-10 11:20:51 +0300

    add new tests on table view holder

--- application/myBestShopTests/TableViewHolderTest.cs
+++ application/myBestShopTests/TableViewHolderTest.cs
@@ -47,5 +47,57 @@ namespace myBestShopTests
                 Assert.IsNotNull(cell);
             }
         }
+
+        [TestMethod]
+        public void TableViewHolder_allCellsGenerated_Check()
+        {
+            TableViewHolder holder = new TableViewHolder(300, 300, 0, 0);
+            List<ComputerWrapper> computers = new List<ComputerWrapper>();
+            for (int i = 0; i < 20; i++)
+            {
+                var computer = new ComputerWrapper(5, ComputerStatus.IS_USED);
+                computers.Add(computer);
+            }
+            var cells = holder.clear().appendCellsWithStatuses(computers).getCells();
+
+            var statusCount = 0;
+            var infoCount = 0;
+            var hintCount = 0;
+            var cancelCount = 0;
+            var otherCount = 0;
+            foreach (var cell in cells)
+            {
+                var splittedCellName = cell.Name.Split("_");
+               
+                if (splittedCellName[2] == TableViewHolder.statusItemNamePrefix.Split("_")[2])
+                {
+                    statusCount++;
+                } else if (splittedCellName[2] == TableViewHolder.infoItemNamePrefix.Split("_")[2])
+                {
+                    infoCount++;
+                } else if (splittedCellName[2] == TableViewHolder.hintItemNamePrefix.Split("_")[2])
+                {
+                    hintCount++;
+                } else if (splittedCellName[2] == TableViewHolder.cancelItemNamePrefix.Split("_")[2])
+                {
+                    cancelCount++;
+                } else
+                {
+                    Logger.println("_" + splittedCellName[2] + "_");
+                    Logger.println("_" + TableViewHolder.infoItemNamePrefix.Split("_")[2] + "_");
+                    Logger.println("_" + TableViewHolder.hintItemNamePrefix.Split("_")[2] + "_");
+                    Logger.println("_" + TableViewHolder.cancelItemNamePrefix.Split("_")[2] + "_");
+                    Logger.println("_" + TableViewHolder.statusItemNamePrefix.Split("_")[2] + "_");
+                    otherCount++;
+                }
+            }
+
+            Assert.AreEqual(0, otherCount);
+            Assert.AreEqual(0, cells.Count % 4);
+            Assert.AreEqual(cells.Count / 4, infoCount);
+            Assert.AreEqual(cells.Count / 4, statusCount);
+            Assert.AreEqual(cells.Count / 4, cancelCount);
+            Assert.AreEqual(cells.Count / 4, hintCount);
+        }
     }
 }
